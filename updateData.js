b2448cbbf7d8d8d685647c0443311d4514018e9f
yankfy <yankfy@qq.com>
2018-02-26 20:48:22 +0800

    update数组修改器

--- updateData.js
+++ updateData.js
@@ -16,6 +16,29 @@ var db = connect("company")
 // db.company.update({name:"XXX"},workMate3)
 
 // $set 更新
-db.company.update({name: "XXX"}, {"$set": {sex: 0,age: 28}})
+// db.company.update({name: "XXX"}, {"$set": {sex: 0,age: 28}})
+
+// $push更新
+// db.company.update({name:"XXX"},{$push:{interest:"draw"}})
+
+// db.company.update({name:"Pony"},{$push:{"skill.skillFour":'react'}})
+
+// $ne 有则不添加 没有则添加
+// db.company.update({name:"Pony",interest:{$ne:'photoshop'}},{$push:{interest:"Internet"}})
+
+// $addToSet 更简化的添加 有则添加 无则不添加
+// db.company.update({name:"Pony"},{$addToSet:{interest:"Read"}})
+
+// $each 批量修改器
+// var newInterests = ['Sing','Dance','Code']
+// db.company.update({name:"Pony"},{$addToSet:{interest:{$each:newInterests}}})
+
+// $pop 删除修改器 
+// 1 从末端进行删除
+// -1 从开始位置进行删除
+db.company.update({name:"Pony"},{$pop:{interest:1}})
+
+// 数组定位修改
+db.company.update({name:"Pony"},{$set:{"interest.1":"README"}})
 
 print("[updateDatabase] This uodate database is successful")
