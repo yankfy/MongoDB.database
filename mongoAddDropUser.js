4e928ada49a314dc9d036795c530609d95d7f50d
yankfy <yankfy@qq.com>
2018-03-04 22:36:57 +0800

    mongoDB的用户管理

--- mongoAddDropUser.js
+++ mongoAddDropUser.js
@@ -23,4 +23,11 @@ db.system.users.remove({user:"Pony"})
 // 用户鉴权
 db.auth("Pony","123456") // 返回1 鉴权成功 返回0 鉴权失败
 
-// 
+// 启动鉴权
+// mongod --auth
+
+// 连接鉴权
+// mongo -u Pony -p 123456 127.0.0.1:27017/admin
+
+// 连接数据库
+// use “有权限的那个库”
