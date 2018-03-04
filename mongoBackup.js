3f4fd4bfe2459ed363de7edc64cfcc796c163f44
yankfy <yankfy@qq.com>
2018-03-04 22:46:10 +0800

    mongoDB的数据备份和恢复

---
+++ mongoBackup.js
@@ -0,0 +1,4 @@
+// 数据备份
+mongodump --host 127.0.0.1 --port 27017 --out /Users/pony/Documents/Code.Learn/MongoDB.database/
+// 数据恢复
+mongorestore -host 127.0.0.1 --port 27017  /Users/pony/Documents/Code.Learn/MongoDB.database/
